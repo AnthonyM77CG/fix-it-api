98e5926d337009d3b90ae269550ae410494d9ca5
Anthony <anthony77cg@hotmail.com>
2026-05-25 21:40:13 -0500

    Primer commit

---
+++ src/main/java/com/fixit/fixitapi/FixitapiApplication.java
@@ -0,0 +1,13 @@
+package com.fixit.fixitapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixitapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FixitapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fixit/fixitapi/FixitapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fixit.fixitapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixitapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

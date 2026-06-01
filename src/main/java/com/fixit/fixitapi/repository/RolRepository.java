3a6f27779025b16dcfd49e7d36a3fb6cc2d90698
Anthony <anthony77cg@hotmail.com>
2026-06-01 16:16:41 -0500

    feat: repositorios JPA para Area, Rol y Usuario

---
+++ src/main/java/com/fixit/fixitapi/repository/RolRepository.java
@@ -0,0 +1,9 @@
+package com.fixit.fixitapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fixit.fixitapi.model.Rol;
+
+public interface RolRepository extends JpaRepository<Rol, Long> {
+
+}

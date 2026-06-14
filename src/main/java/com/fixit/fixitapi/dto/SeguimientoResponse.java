7813031bc9a7e939c9a719e54aa17fb73e0891a6
Anthony <anthony77cg@hotmail.com>
2026-06-13 21:43:36 -0500

    feat: colocar anotaciones en la clase para los constructores

--- src/main/java/com/fixit/fixitapi/dto/SeguimientoResponse.java
+++ src/main/java/com/fixit/fixitapi/dto/SeguimientoResponse.java
@@ -1,14 +1,20 @@
 package com.fixit.fixitapi.dto;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class SeguimientoResponse {
     private String estado;
     private LocalDateTime fecha;
     private String comentario;
+    private List<String> imagenes;
 }
